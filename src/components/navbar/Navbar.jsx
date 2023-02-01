755d796529649de20d0f52c9fede5ee54c8754d5
iljah <ilja200303@gmail.com>
2023-02-01 12:15:51 +0200

    some size changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,22 +6,24 @@ function Navbar(props) {
     const [mouseOver, setMouseOver] = useState(false);
 
     return (
-        <div className="navbar-container">
+        <div className="navbar-box">
+            <div className="navbar-container">
 
-
-            <div className="navbar-logo">
+                <div className="navbar-logo">
                 <span>
                     <IoIosConstruct size={30}/>
                     Semarim
                 </span>
-            </div>
-            <div className="navbar-links_container">
-                <div className="navbar-links_wrapper">
-                    <Burger/>
+                </div>
+                <div className="navbar-links_container">
+                    <div className="navbar-links_wrapper">
+                        <Burger/>
 
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 }
 
